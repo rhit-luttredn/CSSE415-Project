fd06b87ed1e865d2a07bb20ad27dc9d2a5bdcb1c
rhit-luttredn <luttredn@rose-hulman.edu>
2024-04-23 22:54:13 -0400

    fix small oops

--- cart-pole-dqn.py
+++ cart-pole-dqn.py
@@ -43,7 +43,7 @@ class Args:
     # Algorithm specific arguments
     env_id: str = "CartPole-v1"
     """the id of the environment"""
-    total_timesteps: int = 50_000
+    total_timesteps: int = 300_000
     """total timesteps of the experiments"""
     learning_rate: float = 2.5e-4
     """the learning rate of the optimizer"""
