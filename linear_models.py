2f6406b3e0b3940101d962a2fa57f674f0f1c3b7
Sam Polonus <polonusb@rose-hulman.edu>
2024-05-02 02:19:29 -0400

    Added td3_continuous training

--- linear_models.py
+++ linear_models.py
@@ -33,3 +33,19 @@ class LogisticRegressor(nn.Module):
         x = self.layer_norm(x)
         linear = torch.matmul(x, self.weight) + self.bias
         return torch.softmax(linear, dim=1)
+
+
+class MultiTargetLinearRegressor(nn.Module):
+    def __init__(self, n_features, n_targets, standardize=True):
+        super(MultiTargetLinearRegressor, self).__init__()
+        # Initialize parameters: weight vector and bias
+        self.weight = nn.Parameter(torch.randn(n_features, n_targets))
+        self.bias = nn.Parameter(torch.randn(n_targets))
+        if standardize:
+            self.layer_norm = nn.LayerNorm(n_features)
+        else:
+            self.layer_norm = nn.Identity()
+
+    def forward(self, x):
+        # Linear prediction function
+        return torch.matmul(x, self.weight) + self.bias
